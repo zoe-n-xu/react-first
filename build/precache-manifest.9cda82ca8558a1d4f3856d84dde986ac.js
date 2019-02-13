1605bf565ce52471b7654987f292a678a09191e9
Zoe Xu <zxu@freshsquaredapps.com>
2019-02-14 12:17:21 +1300

    Sync Build

---
+++ build/precache-manifest.9cda82ca8558a1d4f3856d84dde986ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5134c7a6cadd5bbe95db",
+    "url": "/static/js/main.5134c7a6.chunk.js"
+  },
+  {
+    "revision": "62bfe49161c1cfcf0f27",
+    "url": "/static/js/1.62bfe491.chunk.js"
+  },
+  {
+    "revision": "5134c7a6cadd5bbe95db",
+    "url": "/static/css/main.b31d0bc0.chunk.css"
+  },
+  {
+    "revision": "62bfe49161c1cfcf0f27",
+    "url": "/static/css/1.ad201fdf.chunk.css"
+  },
+  {
+    "revision": "018b67377a0177bd094ab6e91059cb55",
+    "url": "/index.html"
+  }
+];
